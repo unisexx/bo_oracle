e5b43fc9ada357282dbbc5fcb21332370f1f8df9
unknown <Favourite@.(none)>
2014-02-26 16:27:13 +0700

    report

--- themes/budget/_header.php
+++ themes/budget/_header.php
@@ -9,47 +9,47 @@
 <ul id="navmenu-h">
         <li><a href="#">บันทึก +</a>
           <ul style="width:200px;">
-            <?php if(permission('budget_request', 'canview')): ?><li><a href="budget_request">รายการคำของบประมาณ</a></li><?php endif;?>            
+            <?php if(permission('budget_request', 'canview')): ?><li><a href="budget_request">รายการคำของบประมาณ</a></li><?php endif;?>
           </ul>
         </li>
         <? if(login_data('budgetadmin')=='on'): ?>
         <li><a href="#">ผู้ดูแลระบบ +</a>
           <ul style="width:200px;">
-            <li><a href="budget_request_commit">ตรวจสอบรายการคำขอ</a>            
+            <li><a href="budget_request_commit">ตรวจสอบรายการคำขอ</a>
           </ul>
-        </li>  
-        <? endif;?>      
+        </li>
+        <? endif;?>
         <?php if(permission('report', 'canview')): ?>
         <li><a href="#">รายงาน +</a>
-            <ul style="width:250px;">      			
+            <ul style="width:250px;">
         			 <li style="width:450px;"><a href="#" style="color:#000;">&gt; ชุดตารางแสดงภาพรวมงบประมาณ</a></li>
                      <li style="width:450px;"><a href="budget_report_1" class="a_report_link">ตารางแสดงความเชื่อมโยง</a></li>
-                     <li style="width:450px;"><a href="report_2.php" class="a_report_link">สรุปงบประมาณรายจ่ายประจำปี จำแนกตามผลผลิตโครงการ</a></li>              
-                     <li style="width:450px;"><a href="report_9.php" class="a_report_link">แผนงบประมาณรายจ่ายประจำปีงบประมาณ</a></li>
-                     <li style="width:450px;"><a href="report_10.php" class="a_report_link">การประมาณการรายจ่ายล่วงหน้าระยะปานกลาง</a></li>
-        			 <li style="width:450px;"><a href="#" style="color:#000;">&gt; ชุดแสดงรายละเอียดจำแนกประเภทรายการ</a></li>                     
-                     <li style="width:450px;"><a href="report_6.php" class="a_report_link">ตารางแสดงคำของบประมาณระดับโครงการ/งบรายจ่าย</a></li>
-        			 <li style="width:450px;"><a href="#" style="color:#000;">&gt; ชุดตารางแสดงรายละเอียดจำแนกตามจังหวัด</a></li>                
-                     <li style="width:450px;"><a href="report_5.php" class="a_report_link">แผนการจัดสรรงบประมาณไปจังหวัด</a></li>
-					 <li style="width:450px;"><a href="#" style="color:#000;">&gt; ชุดตารางแสดงรายละเอียดจำแนกรายไตรมาส</a></li>                
-					 <li style="width:450px;"><a href="report_3.php" class="a_report_link">แผนการใช้จ่ายงบประมาณจำแนกตามรายจ่ายประจำปีงบประมาณ</a></li>
-                     <li style="width:450px;"><a href="report_8.php" class="a_report_link">แผนการปฏิบัติงานและแผนการใช้จ่ายงบประมาณรายจ่ายประจำปีงบประมาณ</a></li>                     
+                     <li style="width:450px;"><a href="report_2.php" class="a_report_link">สรุปงบประมาณรายจ่ายประจำปี จำแนกตามผลผลิตโครงการ</a></li>
+                     <li style="width:450px;"><a href="budget_report/index/9" class="a_report_link">แผนงบประมาณรายจ่ายประจำปีงบประมาณ</a></li>
+                     <li style="width:450px;"><a href="budget_report/index/10" class="a_report_link">การประมาณการรายจ่ายล่วงหน้าระยะปานกลาง</a></li>
+        			 <li style="width:450px;"><a href="#" style="color:#000;">&gt; ชุดแสดงรายละเอียดจำแนกประเภทรายการ</a></li>
+                     <li style="width:450px;"><a href="budget_report/index/6" class="a_report_link">ตารางแสดงคำของบประมาณระดับโครงการ/งบรายจ่าย</a></li>
+        			 <li style="width:450px;"><a href="#" style="color:#000;">&gt; ชุดตารางแสดงรายละเอียดจำแนกตามจังหวัด</a></li>
+                     <li style="width:450px;"><a href="budget/report/index/5" class="a_report_link">แผนการจัดสรรงบประมาณไปจังหวัด</a></li>
+					 <li style="width:450px;"><a href="#" style="color:#000;">&gt; ชุดตารางแสดงรายละเอียดจำแนกรายไตรมาส</a></li>
+					 <li style="width:450px;"><a href="budget_report/index/3" class="a_report_link">แผนการใช้จ่ายงบประมาณจำแนกตามรายจ่ายประจำปีงบประมาณ</a></li>
+                     <li style="width:450px;"><a href="budget_report/index/8" class="a_report_link">แผนการปฏิบัติงานและแผนการใช้จ่ายงบประมาณรายจ่ายประจำปีงบประมาณ</a></li>
                 	 <?php if(permission('logfile', 'canview')): ?><li style="width:450px;"><a href="budget_log" target="_blank">Log File</a></li><?php endif;?>
             </ul>
         </li>
         <?php endif;?>
-		
+
 		<?
 		if(
 		permission('streategy','canview') ||
 		permission('budgettype','canview') ||
 		permission('asset','canview') ||
-		permission('time','canview') 
+		permission('time','canview')
 		)
-		{ 
+		{
 		?>
         <li><a href="#">ตั้งค่า +</a>
-            <ul style="width:180px;">                
+            <ul style="width:180px;">
                 		<?php if(permission('streategy','canview')):?><li><a href="budget_plan">แผนงบประมาณ</a></li><? endif;?>
 						<?php if(permission('budgettype','canview')):?><li><a href="budget_type">หมวดงบประมาณ</a></li><? endif;?>
 						<?php if(permission('asset','canview')):?><li><a href="budget_asset">ครุภัณฑ์</a></li><? endif;?>
