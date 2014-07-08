335d12fe98237e11c58975aea60a29a40162e5fc
unknown <DeVo@.(none)>
2014-07-08 15:16:53 +0700

    sit update mds

--- application/modules/mds_set_position/controllers/mds_set_position.php
+++ application/modules/mds_set_position/controllers/mds_set_position.php
@@ -40,7 +40,7 @@ Class Mds_set_position extends  Mdevsys_Controller{
 		if(is_permit(login_data('id'),1) == '')redirect("mds"); // ตรวจสอบว่าเป็น กพร. หรือไม่
 		
 		if($id != ''){
-				$chk_position = "select * from mds_set_permission_dtl where mds_set_position_id = '".$id."'";
+				$chk_position = "select * from mds_set_permission where mds_set_position_id = '".$id."'";
 				$result_chk_position =  $this->permission_dtl->get($chk_position);
 				$num_chk = count($result_chk_position);
 			if($num_chk == '0'){
